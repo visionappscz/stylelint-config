4cbe693923fb2e568cc4f688800e04a58b3ddd31
Bedřich Schindler <bedrich@visionapps.cz>
2019-09-14 11:27:26 +0200

    Update dependencies (#22)

--- __tests__/index.js
+++ __tests__/index.js
@@ -16,13 +16,13 @@ describe('flags no warnings with valid CSS', () => {
   });
 
   it('did not error', () => (
-    result.then(data => (
+    result.then((data) => (
       expect(data.errored).toBeFalsy()
     ))
   ));
 
   it('flags no warnings', () => (
-    result.then(data => (
+    result.then((data) => (
       expect(data.results[0].warnings.length).toBe(0)
     ))
   ));
@@ -39,43 +39,43 @@ describe('flags warnings with invalid CSS', () => {
   });
 
   it('did error', () => (
-    result.then(data => (
+    result.then((data) => (
       expect(data.errored).toBeTruthy()
     ))
   ));
 
   it('flags one warning', () => (
-    result.then(data => (
+    result.then((data) => (
       expect(data.results[0].warnings.length).toBe(1)
     ))
   ));
 
   it('correct warning text', () => (
-    result.then(data => (
+    result.then((data) => (
       expect(data.results[0].warnings[0].text).toBe('Unexpected !important (declaration-no-important)')
     ))
   ));
 
   it('correct rule flagged', () => (
-    result.then(data => (
+    result.then((data) => (
       expect(data.results[0].warnings[0].rule).toBe('declaration-no-important')
     ))
   ));
 
   it('correct severity flagged', () => (
-    result.then(data => (
+    result.then((data) => (
       expect(data.results[0].warnings[0].severity).toBe('error')
     ))
   ));
 
   it('correct line number', () => (
-    result.then(data => (
+    result.then((data) => (
       expect(data.results[0].warnings[0].line).toBe(2)
     ))
   ));
 
   it('correct column number', () => (
-    result.then(data => (
+    result.then((data) => (
       expect(data.results[0].warnings[0].column).toBe(18)
     ))
   ));
