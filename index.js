02c630db9c0b77a7458b556e0e046c2690ba97dc
Adam Kudrna <adam@visionapps.cz>
2021-11-19 18:07:38 +0100

    BC: Remove duplicate and conflicting rules
    Conflicting rules removed:
    
    - `function-url-quotes` — quotes are now required (it's better to require quotes that work for every use case, including URLs with spaces etc.)
    - `string-quotes` — double quotes are now required (single quotes are incompatible with Sass messages)

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 module.exports = {
   extends: 'stylelint-config-standard',
   rules: {
-    // Override stylelint-config-standard
+
+    // Disable ESlint as we intentionally break alphabetical order rule here.
+    /* eslint-disable sort-keys */
+
+    // Override stylelint-config-standard:
     indentation: 4,
 
-    // Extend with more strict rules
-    // (Disable ESlint as we intentionally break alphabetical order rule here.)
-    'at-rule-no-vendor-prefix': true, // eslint-disable-line sort-keys
+    // Extend with more strict rules:
     'declaration-no-important': true,
     'declaration-property-value-disallowed-list': [
       {
@@ -23,30 +25,17 @@ module.exports = {
           + 'not allowed (declaration-property-value-disallowed-list)',
       },
     ],
-    'font-family-name-quotes': 'always-where-recommended',
-    'function-url-quotes': 'never',
-    'max-line-length': 120,
     'max-nesting-depth': [
       2, {
         ignore: ['blockless-at-rules'],
       },
     ],
-    'media-feature-name-no-vendor-prefix': true,
-    'property-no-vendor-prefix': true,
-    'selector-attribute-quotes': 'always',
     'selector-max-compound-selectors': 3,
     'selector-max-id': 0,
     'selector-max-specificity': '0,4,0',
     'selector-max-universal': 0,
-    'selector-nested-pattern': [
-      '^&:',
-      {
-        message: 'Only pseudo-classes and pseudo-elements can be nested (selector-nested-pattern)',
-      },
-    ],
     'selector-no-qualifying-type': true,
-    'selector-no-vendor-prefix': true,
-    'string-quotes': 'single',
-    'value-no-vendor-prefix': true,
+
+    /* eslint-enable sort-keys */
   },
 };
