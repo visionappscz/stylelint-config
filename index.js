f6d02ed9572d60bf83a0f241226f33a5f22cef04
Adam Kudrna <adam@visionapps.cz>
2021-11-25 00:26:04 +0100

    Explain additional rules in the main config

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ module.exports = {
     // Disable ESlint as we intentionally break alphabetical order rule here.
     /* eslint-disable sort-keys */
 
-    // Override stylelint-config-standard:
-    indentation: 4,
+    // Override `stylelint-config-standard`:
+    indentation: 4, // Long lines are rare in CSS so we can afford bigger indentation.
 
     // Extend with more strict rules:
-    'declaration-no-important': true,
+    'declaration-no-important': true, // Except for utility classes and third-party overrides, !important can be avoided.
     'declaration-property-value-disallowed-list': [
       {
-        '/^background/': [
+        '/^background/': [ // Background images should be stored locally.
           'http:',
           'https:',
         ],
-        '/^transition/': [
+        '/^transition/': [ // Only hardware-accelerable properties should be transitioned.
           '/all/',
         ],
       },
@@ -25,16 +25,16 @@ module.exports = {
           + 'not allowed (declaration-property-value-disallowed-list)',
       },
     ],
-    'max-nesting-depth': [
+    'max-nesting-depth': [ // Because too complex nesting is hard to read.
       2, {
         ignore: ['blockless-at-rules'],
       },
     ],
-    'selector-max-compound-selectors': 3,
-    'selector-max-id': 0,
-    'selector-max-specificity': '0,4,0',
-    'selector-max-universal': 0,
-    'selector-no-qualifying-type': true,
+    'selector-max-compound-selectors': 3, // Keep selector specificity as low as possible by default.
+    'selector-max-id': 0, // In most cases, IDs are for JavaScript, not CSS.
+    'selector-max-specificity': '0,4,0', // Keep selector specificity as low as possible by default.
+    'selector-max-universal': 0, // Most of the time, we should know what elements or classes we are targeting.
+    'selector-no-qualifying-type': true, // In most cases, it only needlessly increases selector specificity.
 
     /* eslint-enable sort-keys */
   },
