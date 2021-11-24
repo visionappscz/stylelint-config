407b80dcfd155da6a847371ba54a8d34e118d6f9
Adam Kudrna <adam@visionapps.cz>
2021-11-25 00:28:14 +0100

    Add config for SCSS syntax that extends `stylelint-config-standard-scss`

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
 const stylelint = require('stylelint');
-const config = require('..');
+const configMain = require('..');
+const configOrder = require('../order');
+const configScss = require('../scss');
 
 const validCss = fs.readFileSync('./__tests__/css-valid.css', 'utf-8');
+const validScss = fs.readFileSync('./__tests__/scss-valid.scss', 'utf-8');
 const invalidCss = fs.readFileSync('./__tests__/css-invalid.css', 'utf-8');
 
 describe('flags no warnings with valid CSS', () => {
@@ -11,7 +14,53 @@ describe('flags no warnings with valid CSS', () => {
   beforeEach(() => {
     result = stylelint.lint({
       code: validCss,
-      config,
+      config: configMain,
+    });
+  });
+
+  it('did not error', () => (
+    result.then((data) => (
+      expect(data.errored).toBeFalsy()
+    ))
+  ));
+
+  it('flags no warnings', () => (
+    result.then((data) => (
+      expect(data.results[0].warnings.length).toBe(0)
+    ))
+  ));
+});
+
+describe('flags no warnings with valid CSS order', () => {
+  let result;
+
+  beforeEach(() => {
+    result = stylelint.lint({
+      code: validCss,
+      config: configOrder,
+    });
+  });
+
+  it('did not error', () => (
+    result.then((data) => (
+      expect(data.errored).toBeFalsy()
+    ))
+  ));
+
+  it('flags no warnings', () => (
+    result.then((data) => (
+      expect(data.results[0].warnings.length).toBe(0)
+    ))
+  ));
+});
+
+describe('flags no warnings with valid SCSS', () => {
+  let result;
+
+  beforeEach(() => {
+    result = stylelint.lint({
+      code: validScss,
+      config: configScss,
     });
   });
 
@@ -34,7 +83,7 @@ describe('flags warnings with invalid CSS', () => {
   beforeEach(() => {
     result = stylelint.lint({
       code: invalidCss,
-      config,
+      config: configMain,
     });
   });
 
